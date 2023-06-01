123ac0894e9de0a3942d79bb48f18082d917cbf0
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-06-01 14:55:03 -0600

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Introduction - Leonardo Céspedes</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <div class="content">
+        <h1>Full Web Development</h1>
+        <a href="http://www.codigoconjuan.com" target="_blank">codigoconjuan.com</a>
+        <p>Introduction to OOP using PHP</p>
+        <p class="hashtag">#FullStackDev</p>
+
+        <div class="result">
