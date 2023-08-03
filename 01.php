4ecd20477a33857f7a1f298e35b9916ede9e70c9
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-08-03 09:43:27 -0600

    Class Methods

--- 01.php
+++ 01.php
@@ -4,25 +4,29 @@ include 'includes/header.php';
 
 // Define a class
 class Product {
-    public $name;
-    public $price;
-    public $available;
+    // public $name;
+    // public $price;
+    // public $available;
     // Create a constructor
-    public function __construct(string $name, int $price, bool $available)
-    {
-        // Assign values
-        $this->name = $name;
-        $this->price = $price;
-        $this->available = $available;
-    }
+    // public function __construct(string $name, int $price, bool $available)
+    // {
+    //     // Assign values
+    //     $this->name = $name;
+    //     $this->price = $price;
+    //     $this->available = $available;
+    // }
     // IN PHP 8.0 we can use this syntax to define the constructor
-    // public function __construct(public string $name, public int $price, public bool $available) {}
+    public function __construct(public string $name, public int $price, public bool $available){}
+    public function showInfo() {
+        echo "The product is: " . $this->name . " and the price is: " . $this->price;
+    }
 }
 
 $product = new Product('Tablet', 2000, true);
-$product->name = 'Tablet';
-$product->price = 2000;
-$product->available = true;
+$product->showInfo();
+// $product->name = 'Tablet';
+// $product->price = 2000;
+// $product->available = true;
 
 echo "<pre>";
 var_dump($product);
