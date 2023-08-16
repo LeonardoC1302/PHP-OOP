306353a28248547b15b7871eab00741d4e989ab7
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-08-16 13:27:47 -0600

    Interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php';
 
+interface TransportInterface {
+    public function getInfo() : string;
+    public function getWheels() : int;
+}
+class Transport implements TransportInterface{
+    public function __construct(protected int $wheels, protected int $capacity){ 
+    }
+    public function getInfo() : string {
+        return "The transport has " . $this->wheels . " wheels and a capacity of " . $this->capacity . " people.";
+    }
+    public function getWheels() : int {
+        return $this->wheels;
+    }
+}
 
 include 'includes/footer.php';
