489375ef9c148a8ec78ef3e68b46bfa3d8a93dbb
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-08-23 15:49:30 -0600

    Connecting with MySQL

--- 09.php
+++ 09.php
@@ -1,4 +1,19 @@
 <?php include 'includes/header.php';
 
+// Connect to DB with mysqli
+$db = new mysqli('localhost', 'root', '123', 'realstate_crud');
+$query = "SELECT title FROM properties";
+/* $result = $db->query($query);
+while($row = $result->fetch_assoc()) {
+    var_dump($row);
+} */
+// Prepared Statement
+$stmt = $db->prepare($query); // DB prepares for query
+$stmt->execute(); // DB executes query
+$stmt->bind_result($title); // DB binds result to variable, does not return result
+
+while($stmt->fetch()) { // DB fetches result
+    echo $title . '<br>';
+}
 
 include 'includes/footer.php';
